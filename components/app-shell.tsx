fc48716ee129e61824eefc08a73f6840a1dfd1b4
Bruno Cunha <bruncun@icloud.com>
2026-06-21 15:17:41 -0400

    feat: enhance Link component with accessibility and styling improvements

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -12,7 +12,11 @@ export function AppShell({ headerContent, children }: AppShellProps) {
   return (
     <main className="text-white min-h-screen bg-neutral-900">
       <nav className="flex w-full items-center justify-between p-200 sm:px-300 sm:py-250">
-        <Link href="/" aria-label="FX Checker">
+        <Link
+          href="/"
+          aria-label="FX Checker"
+          className="rounded-8 focus-visible:shadow-[0_0_0_3px_hsl(var(--neutral-700)),0_0_0_4px_hsl(var(--lime-500))] focus-visible:outline-none"
+        >
           <Logo alt="" />
         </Link>
         {headerContent}
