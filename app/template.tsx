969670b5662621044050b139a1fa97aa869e202b
Bruno Cunha <bruncun@icloud.com>
2026-07-09 23:51:57 -0400

    feat: prevent animation on initial render to reduce element render delay

--- app/template.tsx
+++ app/template.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from "react";
 
+import { PageTransition } from "./page-transition";
+
 export default function RootTemplate({ children }: { children: ReactNode }) {
-  return <div className="fx-page-in">{children}</div>;
+  return <PageTransition>{children}</PageTransition>;
 }
