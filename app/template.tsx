fd5984c090a6f15339cbe7066b1b570f21e354de
Bruno Cunha <bruncun@icloud.com>
2026-07-15 15:56:35 -0400

    refactor: remove unused page transition component and related styles

--- app/template.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from "react";
-
-import { PageTransition } from "./page-transition";
-
-export default function RootTemplate({ children }: { children: ReactNode }) {
-  return <PageTransition>{children}</PageTransition>;
-}
